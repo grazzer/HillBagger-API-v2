3657bc1e1846868a13b3dc9f832d8764a416b242
grazzer <edwardsgraham@hotmail.co.uk>
2025-03-26 20:37:43 +0000

    Added cors

--- dist/src/app.js
+++ dist/src/app.js
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 import { router as hills } from "./routes/hills.js";
 const app = express();
 app.locals.paginationVolume = 20;
+app.use(cors());
 app.use(hills);
 export { app };
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Express } from "express";
+import cors from "cors";
 import { router as hills } from "./routes/hills.js";
 
 const app: Express = express();
 
 app.locals.paginationVolume = 20;
+
+app.use(cors());
 
 app.use(hills);
 
